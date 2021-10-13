2a5c220342f2cf73fd408e16fdc32ccd417a5ef7
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2021-10-13 22:49:23 +0300

    MotionDetectionWinFormsApp: add logging using a configuration file

--- MotionDetectionWinFormsApp/Program.cs
+++ MotionDetectionWinFormsApp/Program.cs
@@ -1,3 +1,4 @@
+using Serilog;
 using System;
 using System.Windows.Forms;
 
@@ -11,6 +12,10 @@ namespace MotionDetectionWinFormsApp
         [STAThread]
         static void Main ()
         {
+            Log.Logger = new LoggerConfiguration ()
+                .ReadFrom.AppSettings ()
+                .CreateLogger ();
+
             Application.EnableVisualStyles ();
             Application.SetCompatibleTextRenderingDefault (false);
             Application.Run (new Form1 ());
