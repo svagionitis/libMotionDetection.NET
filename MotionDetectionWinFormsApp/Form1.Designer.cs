97f42fa29a12c45ea0f1dbe6dc5b18f73468121b
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2021-11-17 21:17:57 +0200

    Form1.Designer/Form1/MotionDetectionWinFormsApp: add icon for project and form

--- MotionDetectionWinFormsApp/Form1.Designer.cs
+++ MotionDetectionWinFormsApp/Form1.Designer.cs
@@ -17,6 +17,7 @@ namespace MotionDetectionWinFormsApp
         private void InitializeComponent ()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label3 = new System.Windows.Forms.Label();
             this.comboBoxCaptureDevice = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -49,10 +50,10 @@ namespace MotionDetectionWinFormsApp
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 187);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(43, 421);
+            this.label3.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.Size = new System.Drawing.Size(90, 37);
             this.label3.TabIndex = 4;
             this.label3.Text = "label3";
             // 
@@ -60,29 +61,30 @@ namespace MotionDetectionWinFormsApp
             // 
             this.comboBoxCaptureDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxCaptureDevice.FormattingEnabled = true;
-            this.comboBoxCaptureDevice.Location = new System.Drawing.Point(121, 30);
+            this.comboBoxCaptureDevice.Location = new System.Drawing.Point(272, 68);
+            this.comboBoxCaptureDevice.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.comboBoxCaptureDevice.Name = "comboBoxCaptureDevice";
-            this.comboBoxCaptureDevice.Size = new System.Drawing.Size(247, 23);
+            this.comboBoxCaptureDevice.Size = new System.Drawing.Size(551, 45);
             this.comboBoxCaptureDevice.TabIndex = 7;
             this.comboBoxCaptureDevice.SelectedIndexChanged += new System.EventHandler(this.comboBoxCaptureDevice_SelectedIndexChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 32);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(38, 72);
+            this.label5.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 15);
+            this.label5.Size = new System.Drawing.Size(203, 37);
             this.label5.TabIndex = 8;
             this.label5.Text = "Capture Device:";
             // 
             // checkBoxCalculateMotionInfo
             // 
             this.checkBoxCalculateMotionInfo.AutoSize = true;
-            this.checkBoxCalculateMotionInfo.Location = new System.Drawing.Point(544, 67);
-            this.checkBoxCalculateMotionInfo.Margin = new System.Windows.Forms.Padding(1);
+            this.checkBoxCalculateMotionInfo.Location = new System.Drawing.Point(1224, 151);
+            this.checkBoxCalculateMotionInfo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBoxCalculateMotionInfo.Name = "checkBoxCalculateMotionInfo";
-            this.checkBoxCalculateMotionInfo.Size = new System.Drawing.Size(141, 19);
+            this.checkBoxCalculateMotionInfo.Size = new System.Drawing.Size(305, 41);
             this.checkBoxCalculateMotionInfo.TabIndex = 11;
             this.checkBoxCalculateMotionInfo.Text = "Calculate Motion Info";
             this.checkBoxCalculateMotionInfo.UseVisualStyleBackColor = true;
@@ -91,39 +93,39 @@ namespace MotionDetectionWinFormsApp
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(15, 121);
-            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Location = new System.Drawing.Point(34, 272);
+            this.label7.Margin = new System.Windows.Forms.Padding(14, 0, 14, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(240, 15);
+            this.label7.Size = new System.Drawing.Size(535, 37);
             this.label7.TabIndex = 17;
             this.label7.Text = "Motion Pixel Count Threshold PerCent Area:";
             // 
             // textBoxMotionPixelCountThresholdPerCentArea
             // 
-            this.textBoxMotionPixelCountThresholdPerCentArea.Location = new System.Drawing.Point(274, 118);
-            this.textBoxMotionPixelCountThresholdPerCentArea.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.textBoxMotionPixelCountThresholdPerCentArea.Location = new System.Drawing.Point(616, 266);
+            this.textBoxMotionPixelCountThresholdPerCentArea.Margin = new System.Windows.Forms.Padding(14, 16, 14, 16);
             this.textBoxMotionPixelCountThresholdPerCentArea.MaxLength = 4;
             this.textBoxMotionPixelCountThresholdPerCentArea.Name = "textBoxMotionPixelCountThresholdPerCentArea";
-            this.textBoxMotionPixelCountThresholdPerCentArea.Size = new System.Drawing.Size(153, 23);
+            this.textBoxMotionPixelCountThresholdPerCentArea.Size = new System.Drawing.Size(339, 43);
             this.textBoxMotionPixelCountThresholdPerCentArea.TabIndex = 16;
             this.textBoxMotionPixelCountThresholdPerCentArea.TextChanged += new System.EventHandler(this.textBoxMotionPixelCountThresholdPerCentArea_TextChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(19, 71);
-            this.label6.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label6.Location = new System.Drawing.Point(43, 160);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(104, 15);
+            this.label6.Size = new System.Drawing.Size(234, 37);
             this.label6.TabIndex = 15;
             this.label6.Text = "Motion Threshold:";
             // 
             // textBoxMotionThreshold
             // 
-            this.textBoxMotionThreshold.Location = new System.Drawing.Point(274, 69);
-            this.textBoxMotionThreshold.Margin = new System.Windows.Forms.Padding(1);
+            this.textBoxMotionThreshold.Location = new System.Drawing.Point(616, 155);
+            this.textBoxMotionThreshold.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxMotionThreshold.Name = "textBoxMotionThreshold";
-            this.textBoxMotionThreshold.Size = new System.Drawing.Size(153, 23);
+            this.textBoxMotionThreshold.Size = new System.Drawing.Size(339, 43);
             this.textBoxMotionThreshold.TabIndex = 14;
             this.textBoxMotionThreshold.TextChanged += new System.EventHandler(this.textBoxMotionThreshold_TextChanged);
             // 
@@ -133,9 +135,10 @@ namespace MotionDetectionWinFormsApp
             // 
             // selectFileButton
             // 
-            this.selectFileButton.Location = new System.Drawing.Point(438, 30);
+            this.selectFileButton.Location = new System.Drawing.Point(986, 68);
+            this.selectFileButton.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.selectFileButton.Name = "selectFileButton";
-            this.selectFileButton.Size = new System.Drawing.Size(282, 23);
+            this.selectFileButton.Size = new System.Drawing.Size(634, 52);
             this.selectFileButton.TabIndex = 18;
             this.selectFileButton.Text = "Select A Video File...";
             this.selectFileButton.UseVisualStyleBackColor = true;
@@ -150,28 +153,30 @@ namespace MotionDetectionWinFormsApp
             this.inputGroupBox.Controls.Add(this.label5);
             this.inputGroupBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.inputGroupBox.Location = new System.Drawing.Point(0, 0);
+            this.inputGroupBox.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.inputGroupBox.Name = "inputGroupBox";
-            this.inputGroupBox.Size = new System.Drawing.Size(1547, 76);
+            this.inputGroupBox.Padding = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.inputGroupBox.Size = new System.Drawing.Size(3481, 171);
             this.inputGroupBox.TabIndex = 19;
             this.inputGroupBox.TabStop = false;
             this.inputGroupBox.Text = "Input";
             // 
             // rtspURITextBox
             // 
-            this.rtspURITextBox.Location = new System.Drawing.Point(841, 29);
-            this.rtspURITextBox.Margin = new System.Windows.Forms.Padding(1);
+            this.rtspURITextBox.Location = new System.Drawing.Point(1892, 65);
+            this.rtspURITextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rtspURITextBox.Name = "rtspURITextBox";
-            this.rtspURITextBox.Size = new System.Drawing.Size(215, 23);
+            this.rtspURITextBox.Size = new System.Drawing.Size(479, 43);
             this.rtspURITextBox.TabIndex = 19;
             this.rtspURITextBox.TextChanged += new System.EventHandler(this.rtspURITextBox_TextChanged);
             // 
             // rtspURILabel
             // 
             this.rtspURILabel.AutoSize = true;
-            this.rtspURILabel.Location = new System.Drawing.Point(782, 31);
-            this.rtspURILabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.rtspURILabel.Location = new System.Drawing.Point(1760, 70);
+            this.rtspURILabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rtspURILabel.Name = "rtspURILabel";
-            this.rtspURILabel.Size = new System.Drawing.Size(56, 15);
+            this.rtspURILabel.Size = new System.Drawing.Size(129, 37);
             this.rtspURILabel.TabIndex = 20;
             this.rtspURILabel.Text = "RTSP URI:";
             // 
@@ -184,9 +189,11 @@ namespace MotionDetectionWinFormsApp
             this.settingsGroupBox.Controls.Add(this.textBoxMotionPixelCountThresholdPerCentArea);
             this.settingsGroupBox.Controls.Add(this.label6);
             this.settingsGroupBox.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.settingsGroupBox.Location = new System.Drawing.Point(0, 538);
+            this.settingsGroupBox.Location = new System.Drawing.Point(0, 1210);
+            this.settingsGroupBox.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.settingsGroupBox.Name = "settingsGroupBox";
-            this.settingsGroupBox.Size = new System.Drawing.Size(1547, 223);
+            this.settingsGroupBox.Padding = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.settingsGroupBox.Size = new System.Drawing.Size(3481, 502);
             this.settingsGroupBox.TabIndex = 20;
             this.settingsGroupBox.TabStop = false;
             this.settingsGroupBox.Text = "Settings";
@@ -200,9 +207,11 @@ namespace MotionDetectionWinFormsApp
             this.outputGroupBox.Controls.Add(this.label1);
             this.outputGroupBox.Controls.Add(this.capturedImageBox);
             this.outputGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.outputGroupBox.Location = new System.Drawing.Point(0, 76);
+            this.outputGroupBox.Location = new System.Drawing.Point(0, 171);
+            this.outputGroupBox.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.outputGroupBox.Name = "outputGroupBox";
-            this.outputGroupBox.Size = new System.Drawing.Size(1547, 462);
+            this.outputGroupBox.Padding = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.outputGroupBox.Size = new System.Drawing.Size(3481, 1039);
             this.outputGroupBox.TabIndex = 21;
             this.outputGroupBox.TabStop = false;
             this.outputGroupBox.Text = "Output";
@@ -210,10 +219,10 @@ namespace MotionDetectionWinFormsApp
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(544, 76);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(1224, 171);
+            this.label4.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(94, 15);
+            this.label4.Size = new System.Drawing.Size(211, 37);
             this.label4.TabIndex = 12;
             this.label4.Text = "Forground Mask";
             // 
@@ -223,20 +232,20 @@ namespace MotionDetectionWinFormsApp
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.forgroundImageBox.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
-            this.forgroundImageBox.Location = new System.Drawing.Point(547, 116);
-            this.forgroundImageBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.forgroundImageBox.Location = new System.Drawing.Point(1231, 261);
+            this.forgroundImageBox.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
             this.forgroundImageBox.Name = "forgroundImageBox";
-            this.forgroundImageBox.Size = new System.Drawing.Size(463, 284);
+            this.forgroundImageBox.Size = new System.Drawing.Size(1042, 638);
             this.forgroundImageBox.TabIndex = 11;
             this.forgroundImageBox.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1048, 76);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(2358, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.Size = new System.Drawing.Size(104, 37);
             this.label2.TabIndex = 10;
             this.label2.Text = "Motion";
             // 
@@ -246,20 +255,20 @@ namespace MotionDetectionWinFormsApp
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.motionImageBox.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
-            this.motionImageBox.Location = new System.Drawing.Point(1052, 116);
-            this.motionImageBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.motionImageBox.Location = new System.Drawing.Point(2367, 261);
+            this.motionImageBox.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
             this.motionImageBox.Name = "motionImageBox";
-            this.motionImageBox.Size = new System.Drawing.Size(463, 284);
+            this.motionImageBox.Size = new System.Drawing.Size(1042, 638);
             this.motionImageBox.TabIndex = 9;
             this.motionImageBox.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 76);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(81, 171);
+            this.label1.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.Size = new System.Drawing.Size(214, 37);
             this.label1.TabIndex = 8;
             this.label1.Text = "Captured Image:";
             // 
@@ -269,10 +278,10 @@ namespace MotionDetectionWinFormsApp
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.capturedImageBox.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
-            this.capturedImageBox.Location = new System.Drawing.Point(31, 116);
-            this.capturedImageBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.capturedImageBox.Location = new System.Drawing.Point(70, 261);
+            this.capturedImageBox.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
             this.capturedImageBox.Name = "capturedImageBox";
-            this.capturedImageBox.Size = new System.Drawing.Size(477, 284);
+            this.capturedImageBox.Size = new System.Drawing.Size(1073, 638);
             this.capturedImageBox.TabIndex = 7;
             this.capturedImageBox.TabStop = false;
             this.capturedImageBox.Paint += new System.Windows.Forms.PaintEventHandler(this.capturedImageBox_Paint);
@@ -282,13 +291,14 @@ namespace MotionDetectionWinFormsApp
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(216F, 216F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(1547, 761);
+            this.ClientSize = new System.Drawing.Size(3481, 1712);
             this.Controls.Add(this.outputGroupBox);
             this.Controls.Add(this.settingsGroupBox);
             this.Controls.Add(this.inputGroupBox);
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
